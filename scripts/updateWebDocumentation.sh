7cf2d8f00cc710e6ebce98cd07616db87a6c77da
Pierre-Yves Lapersonne <dev@pylapersonne.info>
2026-09-16 19:11:12 +0200

    ci: fix script for web documentation with hosting base path (#1)
    Closes #1
    
    Signed-off-by: Pierre-Yves Lapersonne <dev@pylapersonne.info>

--- scripts/updateWebDocumentation.sh
+++ scripts/updateWebDocumentation.sh
@@ -13,8 +13,9 @@ set -euo pipefail
 DOCUMENTATION_REPO_URL="git@github.com:pylapp/miso-ios-documentation.git"
 DOCUMENTATION_REPO_BRANCH="main"
 
-# The URL hosting the website (GitHub Pages free tier)
-DOCUMENTATION_URL_ONLINE="https://pylapp.github.io/miso-ios-documentation/"
+# The web documentation is hosted on GitHub Pages, with domain https://pylapp.github.io/miso-ios-documentation
+# The Swift DocC tool for GitHub Pages needs only the repo name, and not the full URL
+DOCUMENTATION_URL_ONLINE="miso-ios-documentation"
 
 # Files present in the documentation repository that must NEVER be overwritten
 # by the generated content. They are the "source of truth" version maintained
@@ -197,7 +198,6 @@ swift package \
     --output-path "$DOCUMENTATION_HTML_LOCATION" \
     --transform-for-static-hosting \
     --hosting-base-path "$DOCUMENTATION_URL_ONLINE" \
-    --experimental-transform-for-static-hosting-with-content \
     --warnings-as-errors \
     --symbol-graph-minimum-access-level public
 
